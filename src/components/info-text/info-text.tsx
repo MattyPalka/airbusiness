bbaad77266c9593427fd4f758478f197947e41c7
MateuszPalka <mateuszpalka90@gmail.com>
2022-11-14 18:53:12 +0100

    create info text component

---
+++ src/components/info-text/info-text.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+export const InfoText = ({ children }: Props) => {
+  return <span className="text-slate-800 text-sm font-light">{children}</span>;
+};
