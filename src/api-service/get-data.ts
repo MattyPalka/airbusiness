d546a586de6d07a80856cf3716bfa21659cc92fa
Mateusz Pałka <mateuszpalka90@gmail.com>
2024-10-27 17:17:57 +0100

    move to own BE

--- src/api-service/get-data.ts
+++ src/api-service/get-data.ts
@@ -1,22 +1,15 @@
-import { YELP_API_KEY } from "../yelp-api-key";
-
 const options = {
   method: "GET",
   headers: {
     accept: "application/json",
     "Access-Control-Allow-Origin": "*",
-    Authorization: `Bearer ${YELP_API_KEY}`,
   },
 };
 
-const CORS_ANYWHERE_URL = "https://cors-anywhere.herokuapp.com/";
-const BASE_API_URL = "https://api.yelp.com";
+const CORS_PROXY_URL = "http://localhost:4000/yelp";
 
 export const getData = (path: string, params?: Record<string, string>) => {
   const searchParams = new URLSearchParams(params);
-  const url = new URL(`/v3${path}`, BASE_API_URL);
-  return fetch(
-    `${CORS_ANYWHERE_URL}${url.toString()}?${searchParams.toString()}`,
-    options
-  ).then((res) => res.json());
+  const url = `${CORS_PROXY_URL}${path}?${searchParams.toString()}`;
+  return fetch(url, options).then((res) => res.json());
 };
