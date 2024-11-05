74c62035c05210fd50d5b4072d2c722431b7c3c5
Mateusz Pałka <mateuszpalka90@gmail.com>
2024-11-05 21:02:23 +0100

    add business skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const Root = () => {
   return (
     <div className="p-4 gap-4 flex flex-col">
       <Navbar />
-      <div className="max-w-screen-2xl mx-auto px-4">
+      <div className="max-w-screen-2xl mx-auto px-4 w-full">
         <Outlet />
       </div>
     </div>
--- src/pages/Business.tsx
+++ src/pages/Business.tsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useBusinessDetails } from "../api-service/useBusinessDetails";
+import { InfoText } from "../components/info-text";
 
 export const Business = () => {
   const { id } = useParams();
@@ -11,9 +12,28 @@ export const Business = () => {
       {isLoading ? (
         "Loading"
       ) : (
-        <div>
-          <div>{business?.name}</div>
-          <div>{business?.display_phone}</div>
+        <div className="flex flex-col gap-4 w-full">
+          <div>
+            <h6 className="font-semibold text-xl">{business?.name}</h6>
+          </div>
+          <div>photo</div>
+          <div className="grid grid-cols-[3fr,_1fr]">
+            <div className="flex flex-col">
+              <span className="font-semibold text-md">
+                {business?.location.address1}, {business?.location.city}
+              </span>
+              <InfoText>
+                {business?.categories.map((category, i) => (
+                  <span key={category.alias}>
+                    {category.title}
+                    {i + 1 < business?.categories.length && ", "}
+                  </span>
+                ))}
+              </InfoText>
+            </div>
+            <div>contact</div>
+          </div>
+          <div>map</div>
         </div>
       )}
     </div>
