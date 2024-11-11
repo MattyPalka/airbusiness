53ff8b999d64f149244484e2e007fce3c9d79575
Mateusz Pałka <mateuszpalka90@gmail.com>
2024-11-11 19:52:18 +0100

    clean up modal

---
+++ src/components/modal/modal.tsx
@@ -0,0 +1,46 @@
+import { ReactNode } from "react";
+import { Icon } from "../icon";
+import clsx from "clsx";
+
+interface Props {
+  children: ReactNode;
+  isOpen: boolean;
+  onClose: VoidFunction;
+  title: string;
+}
+
+export const Modal = ({ children, isOpen, onClose, title }: Props) => {
+  return (
+    <div
+      id="static-modal"
+      data-modal-backdrop="static"
+      tabIndex={-1}
+      aria-hidden={isOpen}
+      className={clsx(
+        isOpen ? "flex bg-gray-300 bg-opacity-50" : "hidden",
+        "overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      )}
+    >
+      <div className="relative p-4 w-full max-w-2xl max-h-full">
+        {/* <!-- Modal content --> */}
+        <div className="relative bg-white rounded-lg shadow ">
+          {/* <!-- Modal header --> */}
+          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
+            <h3 className="text-xl font-semibold text-gray-900 ">{title}</h3>
+            <button
+              type="button"
+              onClick={onClose}
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center "
+              data-modal-hide="static-modal"
+            >
+              <Icon icon="Cross" className="w-3 h-3" />
+              <span className="sr-only">Close modal</span>
+            </button>
+          </div>
+          {/* <!-- Modal body --> */}
+          <div className="p-4 md:p-5 space-y-4">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Business.tsx
+++ src/pages/Business.tsx
@@ -5,8 +5,8 @@ import { PhotoGrid } from "../components/photo-grid/photo-grid";
 import { useTranslation } from "react-i18next";
 import { Icon } from "../components/icon";
 import { useState } from "react";
-import clsx from "clsx";
 import { useBusinessReviews } from "../api-service/useBusinessReviews";
+import { Modal } from "../components/modal/modal";
 
 export const Business = () => {
   const { id } = useParams();
@@ -73,76 +73,43 @@ export const Business = () => {
           </div>
         )}
       </div>
-      <div
-        id="static-modal"
-        data-modal-backdrop="static"
-        tabIndex={-1}
-        aria-hidden={reviewsOpen}
-        className={clsx(
-          reviewsOpen ? "flex bg-gray-300 bg-opacity-50" : "hidden",
-          "overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
-        )}
+
+      <Modal
+        isOpen={reviewsOpen}
+        onClose={() => {
+          setReviewsOpen(false);
+        }}
+        title={t("translation:review_header")}
       >
-        <div className="relative p-4 w-full max-w-2xl max-h-full">
-          {/* <!-- Modal content --> */}
-          <div className="relative bg-white rounded-lg shadow ">
-            {/* <!-- Modal header --> */}
-            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
-              <h3 className="text-xl font-semibold text-gray-900 ">
-                {t("translation:review_header")}
-              </h3>
-              <button
-                type="button"
-                onClick={() => {
-                  setReviewsOpen(false);
-                }}
-                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center "
-                data-modal-hide="static-modal"
-              >
-                <Icon icon="Cross" className="w-3 h-3" />
-                <span className="sr-only">Close modal</span>
-              </button>
-            </div>
-            {/* <!-- Modal body --> */}
-            <div className="p-4 md:p-5 space-y-4">
-              {isReviewLoading && (
-                <div role="status" className="max-w-sm animate-pulse">
-                  <div className="h-2.5 bg-gray-200 rounded-full  w-48 mb-4"></div>
-                  <div className="h-2 bg-gray-200 rounded-full  max-w-[360px] mb-2.5"></div>
-                  <div className="h-2 bg-gray-200 rounded-full  mb-2.5"></div>
-                  <div className="h-2 bg-gray-200 rounded-full  max-w-[330px] mb-2.5"></div>
-                  <div className="h-2 bg-gray-200 rounded-full  max-w-[300px] mb-2.5"></div>
-                  <div className="h-2 bg-gray-200 rounded-full  max-w-[360px]"></div>
-                  <span className="sr-only">Loading...</span>
-                </div>
-              )}
-              {reviews?.reviews.map((review) => (
-                <div
-                  key={review.id}
-                  className="border-b pb-4 flex gap-2 flex-col"
-                >
-                  <InfoText>
-                    <div className="flex gap-2 items-center">
-                      <Icon
-                        icon="StarSolid"
-                        className="w-3 h-3 text-yellow-300"
-                      />
-                      {review.rating}
-                      <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
-                      <strong>
-                        {new Date(review.time_created).toLocaleDateString()}
-                      </strong>
-                    </div>
-                  </InfoText>
-                  <p className="text-base leading-relaxed text-gray-500 ">
-                    {review.text}
-                  </p>
-                </div>
-              ))}
-            </div>
+        {isReviewLoading && (
+          <div role="status" className="max-w-sm animate-pulse">
+            <div className="h-2.5 bg-gray-200 rounded-full  w-48 mb-4"></div>
+            <div className="h-2 bg-gray-200 rounded-full  max-w-[360px] mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full  mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full  max-w-[330px] mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full  max-w-[300px] mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full  max-w-[360px]"></div>
+            <span className="sr-only">Loading...</span>
           </div>
-        </div>
-      </div>
+        )}
+        {reviews?.reviews.map((review) => (
+          <div key={review.id} className="border-b pb-4 flex gap-2 flex-col">
+            <InfoText>
+              <div className="flex gap-2 items-center">
+                <Icon icon="StarSolid" className="w-3 h-3 text-yellow-300" />
+                {review.rating}
+                <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
+                <strong>
+                  {new Date(review.time_created).toLocaleDateString()}
+                </strong>
+              </div>
+            </InfoText>
+            <p className="text-base leading-relaxed text-gray-500 ">
+              {review.text}
+            </p>
+          </div>
+        ))}
+      </Modal>
     </>
   );
 };
