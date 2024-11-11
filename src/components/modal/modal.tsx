40da242bf1c965497496f0e02601b4bdd4c2a617
Mateusz Pałka <mateuszpalka90@gmail.com>
2024-11-11 20:52:19 +0100

    add use click outside

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,6 +1,7 @@
-import { ReactNode } from "react";
+import { ReactNode, useRef } from "react";
 import { Icon } from "../icon";
 import clsx from "clsx";
+import { useOnClickOutside } from "usehooks-ts";
 
 interface Props {
   children: ReactNode;
@@ -10,6 +11,10 @@ interface Props {
 }
 
 export const Modal = ({ children, isOpen, onClose, title }: Props) => {
+  const modalRef = useRef(null);
+
+  useOnClickOutside(modalRef, onClose);
+
   return (
     <div
       id="static-modal"
@@ -21,7 +26,7 @@ export const Modal = ({ children, isOpen, onClose, title }: Props) => {
         "overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
       )}
     >
-      <div className="relative p-4 w-full max-w-2xl max-h-full">
+      <div className="relative p-4 w-full max-w-2xl max-h-full" ref={modalRef}>
         {/* <!-- Modal content --> */}
         <div className="relative bg-white rounded-lg shadow ">
           {/* <!-- Modal header --> */}
