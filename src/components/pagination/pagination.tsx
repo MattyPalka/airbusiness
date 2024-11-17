62e1b1207ce9554a41de529ff0e1f28f4da99f3f
Mateusz Pałka <mateuszpalka90@gmail.com>
2024-11-17 13:09:29 +0100

    improve pagination

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, Fragment, SetStateAction, useRef, useState } from "react";
+import { useOnClickOutside } from "usehooks-ts";
 
 interface Props {
   onPageChanged: Dispatch<SetStateAction<number>>;
@@ -12,6 +13,10 @@ export const Pagination = ({
   numOfPages,
   currentPage,
 }: Props) => {
+  const [inputPageNumberOpen, setInputPageNumberOpen] = useState(false);
+  const [inputPageValue, setInputPageValue] = useState<number>(currentPage + 1);
+  const ref = useRef(null);
+  useOnClickOutside(ref, () => setInputPageNumberOpen(false));
   return (
     <nav className="mx-auto">
       <ul className="flex items-center -space-x-px h-8 text-sm">
@@ -40,24 +45,83 @@ export const Pagination = ({
             </svg>
           </button>
         </li>
-        {Array.from(new Array(numOfPages)).map((_, i) => (
-          <li key={i}>
-            <button
-              onClick={() => {
-                onPageChanged(i);
-              }}
-              aria-current="page"
-              className={clsx(
-                "leading-tight px-3 h-8 flex items-center justify-center border",
-                i === currentPage
-                  ? "z-10 text-blue-600  border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 "
-                  : "text-gray-500 bg-white border-gray-300 hover:bg-gray-100 hover:text-gray-700"
-              )}
-            >
-              {i + 1}
-            </button>
-          </li>
-        ))}
+        {Array.from(new Array(numOfPages)).map((_, i) => {
+          if (
+            numOfPages > 20 &&
+            i > 20 &&
+            i < numOfPages - 1 &&
+            currentPage !== i
+          ) {
+            return <Fragment key={i} />;
+          }
+          if (numOfPages > 20 && currentPage !== i && i === 20) {
+            return (
+              <li key={i} className="relative">
+                <button
+                  onClick={() => {
+                    setInputPageNumberOpen(true);
+                  }}
+                  aria-current="page"
+                  className={clsx(
+                    "leading-tight px-3 h-8 flex items-center justify-center border",
+                    i === currentPage
+                      ? "z-10 text-blue-600  border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 "
+                      : "text-gray-500 bg-white border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+                  )}
+                >
+                  ...
+                </button>
+                {inputPageNumberOpen && (
+                  <div
+                    ref={ref}
+                    role="tooltip"
+                    className="absolute text-sm px-3 py-2 rounded shadow-md z-10 bottom-full left-1/2 transform -translate-x-1/2 mb-2"
+                  >
+                    <input
+                      value={inputPageValue}
+                      className="p-2"
+                      type="number"
+                      min={1}
+                      max={numOfPages - 1}
+                      onKeyDown={(e) => {
+                        if (e.key === "Enter" && !!inputPageValue) {
+                          onPageChanged(inputPageValue - 1);
+                          setInputPageValue(0);
+                        }
+                      }}
+                      onChange={(e) => {
+                        if (
+                          Number(e.currentTarget.value) > 0 &&
+                          Number(e.currentTarget.value) < numOfPages - 1
+                        ) {
+                          setInputPageValue(Number(e.currentTarget.value));
+                        }
+                      }}
+                    />
+                  </div>
+                )}
+              </li>
+            );
+          }
+          return (
+            <li key={i}>
+              <button
+                onClick={() => {
+                  onPageChanged(i);
+                }}
+                aria-current="page"
+                className={clsx(
+                  "leading-tight px-3 h-8 flex items-center justify-center border",
+                  i === currentPage
+                    ? "z-10 text-blue-600  border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 "
+                    : "text-gray-500 bg-white border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+                )}
+              >
+                {i + 1}
+              </button>
+            </li>
+          );
+        })}
 
         <li>
           <button
